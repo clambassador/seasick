54ba1374f50864a95108e418fae8b4e4d81e7349
Joel Reardon <joel.reardon@gmail.com>
2019-01-23 15:50:52 -0700

    Added notfound filter

---
+++ csv_filter_notfound.cc
@@ -0,0 +1,39 @@
+#include <cassert>
+#include <iostream>
+#include <set>
+#include <string>
+
+#include "ib/fileutil.h"
+#include "ib/logger.h"
+#include "ib/tokenizer.h"
+#include "ib/csv_table.h"
+
+using namespace ib;
+using namespace std;
+
+int main(int argc, char** argv) {
+	if (argc != 3) {
+		Logger::error("usage: % allowfile colnum", argv[0]);
+		return -1;
+	}
+	set<size_t> cols;
+	Tokenizer::numset(argv[2], &cols, -1);
+	assert(cols.size());
+	set<string> allowed;
+	Fileutil::read_file(argv[1], &allowed);
+	unique_ptr<CSVTable<false>> table;
+	table.reset(new CSVTable<false>());
+	table->stream();
+	vector<string> row;
+	while (table->get_next_row(&row)) {
+		string key = Tokenizer::join(row, ",", cols);
+		if (allowed.count(key)) {
+			Logger::info("found % in %", key, row);
+			allowed.erase(key);
+		}
+		row.clear();
+	}
+	for (auto &x : allowed) {
+		cout << x << endl;
+	}
+}
